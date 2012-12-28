5c2f31aecb350d638e188491d44b9e812a14078f
Jonatas Wesley Gonçalves <johnnyrox@gmail.com>
2012-12-28 12:11:25 -0200

    alterações realizadas para rodar no servidor

---
+++ conteudo/sql/estado.sql
@@ -0,0 +1,53 @@
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Acre','AC','Rio Branco');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Alagoas','AL','Maceió');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Amapá','AP','Macapá');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Amazonas','AM','Manaus');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Bahia','BA','Salvador');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Ceará','CE','Fortaleza');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Distrito Federal','DF','Brasília');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Espírito Santo','ES','Vitória');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Goiás','GO','Goiânia');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Maranhão','MA','São Luís');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Mato Grosso','MT','Cuiabá');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Mato Grosso do Sul','MS','Campo Grande');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Minas Gerais','MG','Belo Horizonte');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Paraná','PR','Curitiba');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Paraíba','PB','João Pessoa');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Pará','PA','Belém');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Pernambuco','PE','Recife');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Piauí','PI','Teresina');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Rio de Janeiro','RJ','Rio de Janeiro');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Rio Grande do Norte','RN','Natal');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Rio Grande do Sul','RS','Porto Alegre');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Rondônia','RO','Porto Velho');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Roraima','RR','Boa Vista');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Santa Catarina','SC','Florianópolis');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Sergipe','SE','Aracaju');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'São Paulo','SP','São Paulo');
+
+insert into conteudo_estado(id,descricao,uf,capital) values (null,'Tocantins','TO','Palmas');
